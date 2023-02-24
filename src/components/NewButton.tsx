724b19531c379420bec6f125514172bf2f39280c
Marko Ristin <marko.ristin@gmail.com>
2023-02-24 17:27:16 +0100

    Add keyboard shortcuts
    We add the keyboard shortcuts (with alt instead of ctrl to avoid blocks
    in browsers). This makes the work with the app a bit less tedious if you
    want to save the file often.

--- src/components/NewButton.tsx
+++ src/components/NewButton.tsx
@@ -1,27 +1,42 @@
 import * as React from 'react'
+import * as valtio from 'valtio'
 
 import * as emptory from "../emptory.generated"
 import * as enhancing from "../enhancing.generated";
 import * as model from "../model"
 import * as widgets from "./widgets"
 
+
 export function NewButton(props: { state: model.State }) {
   const [dialogueMessage, setDialogueMessage] = React.useState<string | null>(
     null
   );
 
+  function showDialogue() {
+    setDialogueMessage(
+      "A new environment will delete all the unsaved changes. " +
+      "Are you really sure?"
+    )
+  }
+
+  const snapState = valtio.useSnapshot(props.state);
+
+  React.useEffect(
+    () => {
+      if (props.state.enqueuedAction === model.ACTION_NEW) {
+        setDialogueMessage(
+          "A new environment will delete all the unsaved changes. " +
+          "Are you really sure?"
+        );
+        props.state.enqueuedAction = null;
+      }
+    },
+    [snapState.enqueuedAction]
+  )
+
   return (
     <>
-      <button onClick={
-        () => {
-          setDialogueMessage(
-            "A new environment will delete all the unsaved changes. " +
-            "Are you really sure?"
-          )
-        }
-      }>
-        New
-      </button>
+      <button onClick={showDialogue}>New</button>
       <widgets.OkCancelScreen
         message={dialogueMessage}
         onOk={
